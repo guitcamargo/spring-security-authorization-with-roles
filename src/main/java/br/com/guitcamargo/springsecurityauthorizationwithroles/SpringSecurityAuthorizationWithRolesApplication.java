8e11d81a030ad6d9e8fb9fec470799b2d5e5bd81
Guilherme Theodoro Camargo <s2it_gcamargo@uolinc.com>
2023-10-28 16:10:37 -0300

    Commit inicial

---
+++ src/main/java/br/com/guitcamargo/springsecurityauthorizationwithroles/SpringSecurityAuthorizationWithRolesApplication.java
@@ -0,0 +1,13 @@
+package br.com.guitcamargo.springsecurityauthorizationwithroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationWithRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthorizationWithRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guitcamargo/springsecurityauthorizationwithroles/SpringSecurityAuthorizationWithRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guitcamargo.springsecurityauthorizationwithroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthorizationWithRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
